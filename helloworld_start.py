772e4789bb0b25bc7c6d35803ae4ebcee90c99fc
it-is-nidah <leonidakoech@gmail.com>
2020-10-28 23:47:43 +0300

    print variables

---
+++ helloworld_start.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+name = input("What is your name? ")
+print(name+", it is so nice to meet ya!")
+age = input("How old are ya? ")
+print("Hi, I am "+name+" and I am "+age+" years old.")
