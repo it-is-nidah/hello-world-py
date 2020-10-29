ed9027cd8e5634aadae3121f4291a442fedd213b
it-is-nidah <leonidakoech@gmail.com>
2020-10-29 15:50:36 +0300

    call a function

--- helloworld_start.py
+++ helloworld_start.py
@@ -1,5 +1,12 @@
-print("Hello World!")
-name = input("What is your name? ")
-print(name+", it is so nice to meet ya!")
-age = input("How old are ya? ")
-print("Hi, I am "+name+" and I am "+age+" years old.")
+
+
+
+def main():
+    print("Hello World!")
+    name = input("What is your name? ")
+    print(name+", it is so nice to meet ya!")
+    age = input("How old are ya? ")
+    print("Hi, I am "+name+" and I am "+age+" years old.")
+
+if __name__ == "__main__":
+    main()
