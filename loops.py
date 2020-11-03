1ebff7321786883af6f3a9f51b22bad9275c4d37
it-is-nidah <leonidakoech@gmail.com>
2020-11-03 22:46:44 +0300

    --no-edit

--- loops.py
+++ loops.py
@@ -6,25 +6,28 @@ def main():
   x = 0
 
   # define a while loop
-  # while (x < 5 ):
-  #   print (x)
+  while (x < 5 ):
+    print (x)
 
-  #   x = x+1
+    x = x+1
 
   # # define a for loop
-  # for x in range( 5, 11):
-  #   print (x)
+  for x in range( 5, 11):
+    print (x)
 
   # # use a for loop over a collection
-  # days = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
-  # for d in days :
-  #     print (d)
+  days = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
+  for d in days :
+    print (d)
   
   # use the break and continue statements
-  # for b in range ( 1, 10):
-  #   # if( b == 7 ): break
-  #   if ( b % 2 == 0) : continue
-  #   print (b)
+  for b in range ( 1, 10):
+    if( b == 7 ): break
+    print (b)
+
+  for y in range ( 1, 10):
+    if ( y % 2 == 0) : continue
+    print (y)
 
   #using the enumerate() function to get index 
   days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
